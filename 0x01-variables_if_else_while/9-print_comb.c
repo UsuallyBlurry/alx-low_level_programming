cb014a29665573ce9c414318ba6a5ea398444542
Idriss Abouhasba <driss_abouhasba@um5.ac.ma>
2023-07-14 16:33:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 	putchar(i + '0');
+	if (i != 9)
+	{
 	putchar(',');
 	putchar(' ');
 	}
-	putchar(i + '0');
+	}
 	putchar('\n');
 	return (0);
 }
